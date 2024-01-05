5564220a6dc789e7f46d0d78f9f2c657b719ea13
Thao Nguyen <73497298+Amasterworld@users.noreply.github.com>
2024-01-05 23:17:53 +0100

    Update Loss.py

--- DeepLearning/exercise2_material/src_to_implement/Optimimization/Loss.py
+++ DeepLearning/exercise2_material/src_to_implement/Optimimization/Loss.py
@@ -10,6 +10,7 @@ class CrossEntropyLoss:
 
     def forward(self, predict_tensor, label_tensor):
         epsilon = np.finfo(float).eps  # Get the machine epsilon
+        #store that last output_tensor of the forward process, that is used for the initial error_tensor in backward process
         self.predict = predict_tensor
         
         # Apply the formula: loss = -sum(label_tensor * log(predict_tensor + eps))
